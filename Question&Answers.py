8fd870a75c26ac12cd19878c1654c3a2ca344f86
Vijay Laxman Rajage <104711672+VJ2151@users.noreply.github.com>
2025-07-18 00:42:08 +0530

    Update Question&Answers.py

--- Question&Answers.py
+++ Question&Answers.py
@@ -31,11 +31,112 @@
 # print(occurence)
 
 # Writing Fibonacci Series:   0, 1, 1, 2, 3, 5, 8, 13, 21, 34
-def fibonacci():
-    num = int(input("enter number till you want fibonacci series: "))
-    a,b = 0,1
-    print("Fibonacci Series:", end=' ')
-    for i in range(num):
-        print(a, end=' ')
-        a,b = b, a+b
-fibonacci()
+# def fibonacci():
+#     a,b = 0,1
+#     num = int(input("enter any no. : "))
+#     for i in range(num):
+#         print(a,end=" ")
+#         a,b = b, a+b
+# fibonacci()
+
+
+# Finding the Maximum Number in a List:
+# list1 = input("Enter items in a list: ").split()
+# biggest = 0
+# for i in list1:
+#     if (int(i) > biggest):
+#         biggest = int(i)
+# print(biggest)
+
+# Finding the minimum Number in a List:
+# list1 = input("Enter items in a list: ").split()
+# smallest = 100
+# for i in list1:
+#     if (int(i) < smallest):
+#         biggest = int(i)
+# print(smallest)
+
+# Finding the 2nd maximum Number in a List:
+# list1 = input("Enter items in a list: ").split()
+# biggest = int(list1[0])
+# second_biggest = int(list1[1])
+# if (second_biggest > biggest):
+#     biggest, second_biggest = second_biggest, biggest
+# else:
+#     pass
+# for i in list1[2:]:
+#     num = int(i)
+#     if num > biggest:
+#         second_biggest = biggest
+#         biggest = num
+#     elif num > second_biggest:
+#         second_biggest = num
+# print("biggest : ",biggest)
+# print("second_biggest : ",second_biggest)
+
+# Finding the Middle Element in a List:
+# list1 = input("Enter items for a list separated by spaces: ").split()
+# n = len(list1)
+
+# if n % 2 == 1:
+#     print("Middle element:", list1[n // 2])
+# else:
+#     print("Middle elements:", list1[(n // 2) - 1], "and", list1[n // 2])
+
+# Converting a List into a String:
+# list1 = ['Hello,', 'My', 'Name', 'is', 'vijay', 'Rajage']
+# str = ' '.join(list1)
+# print(str)
+
+# Adding Two List Elements Together:
+# lst1 = [1, 2, 3]
+# lst2 = [4, 5, 6] 
+# added_list =[]
+# for i in range(0,len(lst1)):
+#         add = lst1[i]+lst2[i]
+#         added_list.append(add)
+# print(added_list)
+
+# Comparing Two Strings for Anagrams:
+# str1 = "Listen"
+# str2 = "Silent"
+# if sorted(str1.upper()) == sorted(str2.upper()):
+#     print("anagrams")
+# else:
+#     print("not anagrams")
+
+# Palindrome(A palindrome is a word, number, phrase, or sequence that reads the same backward as forward.):
+# str1 = "Kayak".lower()
+# str2 = "kayak".lower()
+# if str1[::-1]==str2:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# Counting the White Spaces in a String
+# string = "P r ogramm in g "
+# print(string.count(' '))
+
+# Removing All Whitespace in a String:
+# string = "C O D E"
+# str=string.split()
+# print(''.join(str))
+
+# Building a Pyramid in Python:
+# rows = 6
+# for i in range(rows):
+#     print("_" * (rows - i - 1), end="")           # Print spaces 
+#     print("*" * (2 * i + 1))                      # Print stars
+
+
+
+
+
+
+
+
+
+
+
+
+
